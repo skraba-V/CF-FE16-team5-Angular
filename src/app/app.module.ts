95cabe759df77f227fef3bde0c895341c2fb15be
TamaraZolnajic <zolnajic.tamara@gmail.com>
2022-06-21 16:04:14 +0200

    finished cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
