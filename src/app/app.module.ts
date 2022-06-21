4ee9c17cd00bd8c9f4cda8f68f55c3cffc1ff725
TamaraZolnajic <zolnajic.tamara@gmail.com>
2022-06-21 16:24:42 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FooterComponent } from './footer/footer.component';
-import { ReviewsComponent } from './reviews/reviews.component';
+
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { CardsComponent } from './cards/cards.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    FooterComponent,
-    ReviewsComponent,
     CardsComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    FooterComponent,
+    ReviewsComponent,
+    
 
   ],
   imports: [
